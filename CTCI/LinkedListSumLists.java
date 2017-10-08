70164df52fa7412f69488416d543c79e4a31040d
harishphn <harish.pendela@gmail.com>
2017-10-08 11:48:44 -0700

    LinkedList - SumLists
    CTCI

--- CTCI/LinkedListSumLists.java
+++ CTCI/LinkedListSumLists.java
@@ -91,6 +91,52 @@ class LinkedListSumLists
 
 	public static Node sumLists(Node list1, Node list2)
 	{
-		
+		StringBuilder sb = new StringBuilder();
+
+		Node cur1 = list1;
+		while(cur1!=null)
+		{
+			sb.append(Integer.toString(cur1.data));
+			cur1 = cur1.next;
+		}
+		sb.reverse();
+		int num1 = Integer.parseInt(sb.toString());
+		sb.setLength(0);
+
+		cur1 = list2;
+		while(cur1!=null)
+		{
+			sb.append(Integer.toString(cur1.data));
+			cur1 = cur1.next;
+		}
+		sb.reverse();
+		int num2 = Integer.parseInt(sb.toString());
+		sb.setLength(0);
+
+		int sum = num1+num2;
+		sb.append(sum);
+		sb.reverse();
+		sum = Integer.parseInt(sb.toString());
+		Node list3 = null;
+		int n;
+		while(sum>0)
+		{
+			n = sum % 10;
+			sum = sum/10;
+			Node newNode = new Node();
+			newNode.data = n;
+			newNode.next = null;
+			if(list3 == null)
+			{
+				list3 = newNode;
+			}
+			else
+			{
+				newNode.next = list3;
+				list3 = newNode;
+			}
+		} 
+
+		return list3;
 	}
 }
